ac64ec30c42b3a2388f5e2c7792fea966b280959
Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>
2024-04-23 00:18:01 +0200

    Sprinkle some poor man's debugs

--- extensions/html-language-features/server/src/htmlServer.ts
+++ extensions/html-language-features/server/src/htmlServer.ts
@@ -304,23 +304,32 @@ export function startServer(connection: Connection, runtime: RuntimeEnvironment)
 	connection.onDidChangeTextDocument(async (params) => {
 
 		const document = documents.get(params.textDocument.uri);
+		console.error("A");
 		if (document) {
+			console.error("B");
 			for (const change of params.contentChanges) {
+				console.error("C");
 				if (TextDocumentContentChangeEvent.isIncremental(change)) {
+					console.error("D");
 					const pos = change.range;
 					if (pos.end.line !== pos.start.line || pos.end.character != pos.start.character) {
 						continue;
 					}
+					console.error("E");
 					if (pos.start.character > 0) {
+						console.error("F");
 						const mode = languageModes.getModeAtPosition(document, Position.create(pos.start.line, pos.start.character - 1));
 						if (mode && mode.doAutoInsert) {
+							console.error("G");
 							let result: string | null = null;
 							if (change.text === '=') {
 								result = await mode.doAutoInsert(document, pos.start, 'autoQuote');
 							} else if (change.text == '>') {
 								result = await mode.doAutoInsert(document, pos.start, 'autoClose');
 							}
+
 							if (result !== null) {
+								console.error("H");
 								let snippet: SnippetTextEdit = { range: pos, snippet: StringValue.createSnippet(result), annotationId: undefined };
 								const params: ApplyWorkspaceEditParams = {
 									edit: {
