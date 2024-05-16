3b77c54dfffef3f3213e435f4f549222fe96d22e
Piotr Osiewicz <24362066+osiewicz@users.noreply.github.com>
2024-05-16 17:05:35 +0200

    Simplify htmlServerWorkerMain

--- extensions/html-language-features/server/src/browser/htmlServerWorkerMain.ts
+++ extensions/html-language-features/server/src/browser/htmlServerWorkerMain.ts
@@ -24,7 +24,11 @@ const messageHandler = async (e: any) => {
 		}
 		await import('./htmlServerMain');
 		if (self.onmessage !== messageHandler) {
-			pendingMessages.forEach(msg => self.onmessage?.(msg));
+			pendingMessages.forEach(msg => {
+				if (self.onmessage) {
+					self.onmessage(msg)
+				}
+			});
 			pendingMessages.length = 0;
 		}
 		l10nLog.forEach(console.log);
